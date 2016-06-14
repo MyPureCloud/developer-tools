5033e6e61ccf283c35a812efa8247d1764b10b1d
Kevin Glinski <kevin.glinski@inin.com>
2016-06-14 17:54:36 -0400

    add logout option

---
+++ app/components/nav-tree.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    purecloud: Ember.inject.service(),
+    actions:{
+        logOut(){
+            debugger;
+            this.get('purecloud').get("session").logout();
+
+            /*if(window && window.localStorage){
+                delete window.localStorage.authtoken;
+            }
+
+            window.location.reload();*/
+        }
+    }
+});
