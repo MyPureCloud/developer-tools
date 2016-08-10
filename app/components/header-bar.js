d6786bd98e25d72b7aa8bdffa54d13ac2f08e740
Casey Dexter <casey.dexter@inin.com>
2016-08-09 20:47:16 -0400

    Move left icon bar to top navbar
    Add ININ custom icons
    Add bower components: Bootstrap, Font Awesome, Ion Icons

--- app/components/header-bar.js
+++ app/components/header-bar.js
@@ -12,7 +12,7 @@ export default Ember.Component.extend({
         for(let x=0;x < toolsModules.length; x++){
             let module = toolsModules[x];
             if(module.path === route){
-                return module.name;
+                return "Developer Tools";
             }
         }
 
