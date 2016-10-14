03fe910c4f90272c1ec5981797d842310dfac5b6
Kevin Glinski <kevin.glinski@Inin.com>
2016-10-14 10:33:29 -0400

    user aggregate query (#26)

---
+++ app/components/user-aggregate-query.js
@@ -0,0 +1,54 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    purecloud: Ember.inject.service('purecloud'),
+    filter: null,
+    granularity: null,
+    interval: null,
+    groupBy: ['userId'],
+    selectedMetrics:[],
+    userMetrics:['tSystemPresence', 'tOrganizationPresence', 'tAgentRoutingStatus'],
+    userFilters:['','userId'],
+    init(){
+        this._super(...arguments);
+        this.get('filter');
+    },
+    _computeValue:function(){
+
+        let query={
+
+        };
+
+        var interval = this.get('interval');
+        if(interval){
+            query.interval = interval;
+        }
+
+        var granularity = this.get('granularity');
+        if(granularity){
+            query.granularity = granularity;
+        }
+
+        var groupBy = this.get("groupBy");
+        if(groupBy && groupBy !== ''){
+            query.groupBy = groupBy;
+        }
+
+        var filter = this.get("filter");
+        if(filter){
+            query.filter = filter;
+        }
+
+        var selectedMetrics = this.get('selectedMetrics');
+        if(selectedMetrics && selectedMetrics.length>0){
+            query.metrics = selectedMetrics;
+        }
+
+        return query;
+    },
+    queryJson:null,
+    _observeChanges: Ember.observer('granularity', 'interval', 'groupBy', 'filter', function() {
+        let query = JSON.stringify(this._computeValue(), null, " ");
+        this.set('queryJson', query);
+    })
+});
